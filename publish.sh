fcf87f38e2ac382f1814bcc3813f512716be483f
sahharYoucef <sahharYoucef@gmail.com>
2020-06-08 16:51:57 +0100

    small changes

--- publish.sh
+++ publish.sh
@@ -5,10 +5,4 @@ cd ..
 flutter format lib/
 pub publish --force
 
-<<<<<<< HEAD
-cd assets_audio_player_web
-./publish.sh
-
-=======
->>>>>>> aef903db08b6554d4dee86baea5b9f590778de5b
 git commit -am "published" && git push
